0a394dc1eaee373c3f8481eb80ed7bb50f87b1f3
badbyte0xbadf00d <badbyte0xbedf00d@proton.me>
2023-03-30 11:53:09 +0200

    Added wind and temperature trend lines for city on click on frontend app.

--- Task2/WebApp/giovanniweather/src/App.js
+++ Task2/WebApp/giovanniweather/src/App.js
@@ -7,39 +7,46 @@ import useWeatherData from './data/useWeatherData';
 import './App.css';
 
 const App = () => {
-  const data = useWeatherData('https://localhost:7072/WeatherForecast/GetWeatherForecast');
-
-  const lineChartData = {
-    data: [
-      {
-        id: 'wind',
-        data: data.map((cityData) => ({
-          x: `${cityData.cityName}, ${cityData.countryName}`,
-          y: cityData.wind,
-        })),
-      },
-      {
-        id: 'temperature',
-        data: data.map((cityData) => ({
-          x: `${cityData.cityName}, ${cityData.countryName}`,
-          y: cityData.temperature,
-        })),
-      },
-    ],
-  };
-
   const [chartType, setChartType] = useState('bar');
+  const [cityName, setCityName] = useState('');
+  const [countryName, setCountryName] = useState('');
+
+  const data = useWeatherData('https://localhost:7072/WeatherForecast', chartType, countryName, cityName);
+
+  const lineChartData = () => {
+    return {
+      data: [
+        {
+          id: 'wind',
+          data: data.map((weatherData) => ({
+            x: `${new Date(weatherData.avgUpdateDateTime).toLocaleString()}`,
+            y: Math.round(weatherData.avgWind * 10) / 10,
+          })),
+        },
+        {
+          id: 'temperature',
+          data: data.map((weatherData) => ({
+            x: `${new Date(weatherData.avgUpdateDateTime).toLocaleString()}`,
+            y: Math.round(weatherData.avgTemperature * 10) / 10,
+          })),
+        },
+      ],
+    };
+  };
 
   const toggleChartType = () => {
     setChartType(chartType === 'bar' ? 'line' : 'bar');
   };
 
   const getLatestUpdateTime = () => {
-    const latestUpdate = data.reduce((latest, cityData) => {
-      const cityUpdate = new Date(cityData.lastUpdateDateTime);
-      return latest < cityUpdate ? cityUpdate : latest;
-    }, new Date(0));
-    return latestUpdate.toLocaleString();
+    if (chartType === 'bar') {
+      const latestUpdate = data.reduce((latest, cityData) => {
+        const cityUpdate = new Date(cityData.lastUpdateDateTime);
+        return latest < cityUpdate ? cityUpdate : latest;
+      }, new Date(0));
+      return latestUpdate.toLocaleString();
+    }
+    return new Date(data[data.length - 1].avgUpdateDateTime).toLocaleString();
   };
 
   const temperatureScale = scaleLinear()
@@ -102,63 +109,95 @@ const App = () => {
             </div>
           )}
           onClick={(data, event) => {
+            setCityName(data.data.cityName);
+            setCountryName(data.data.countryName);
             toggleChartType();
           }}
         />
       ) : (
 
-        <ResponsiveLine
-          data={lineChartData.data}
-          isInteractive={true}
-          useMesh={true}
-          margin={{ top: 50, right: 130, bottom: 150, left: 60 }}
-          xScale={{ type: 'point' }}
-          yScale={{
-            type: 'linear',
-            min: 'auto',
-            max: 'auto',
-            stacked: false,
-            reverse: false,
-          }}
-          axisTop={null}
-          axisRight={null}
-          axisBottom={{
-            orient: 'bottom',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: -45,
-            legend: 'City',
-            legendOffset: 100,
-            legendPosition: 'middle',
-          }}
-          axisLeft={{
-            orient: 'left',
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: 'Value',
-            legendOffset: -40,
-            legendPosition: 'middle',
-          }}
-          colors={({ id, value }) =>
-            id === 'temperature' ? 'orange' : 'steelblue'
-          }
-          pointSize={10}
-          pointColor={{ theme: 'background' }}
-          pointBorderWidth={2}
-          pointBorderColor={{ from: 'serieColor' }}
-          pointLabel="y"
-          pointLabelYOffset={-12}
-          enablePointLabel={true}
-          enableArea={false}
-          animate={true}
-          motionStiffness={90}
-          motionDamping={15}
-          onClick={(point, event) => {
-            toggleChartType();
-          }}
-        />
+        <div style={{width: '100%' , height: '700px'}}>
+          <div style={{ textAlign: 'center', marginTop: '10px' }}>
+            Location: {cityName}, {countryName}
+          </div>
+          <ResponsiveLine
+            data={lineChartData().data}
+            isInteractive={true}
+            useMesh={true}
+            margin={{ top: 50, right: 130, bottom: 150, left: 60 }}
+            xScale={{ type: 'point' }}
+            yScale={{
+              type: 'linear',
+              min: 'auto',
+              max: 'auto',
+              stacked: false,
+              reverse: false,
+            }}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+              orient: 'bottom',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: -45,
+              legend: 'Update time',
+              legendOffset: 100,
+              legendPosition: 'middle',
+            }}
+            axisLeft={{
+              orient: 'left',
+              tickSize: 5,
+              tickPadding: 5,
+              tickRotation: 0,
+              legend: 'Value',
+              legendOffset: -40,
+              legendPosition: 'middle',
+            }}
+            colors={({ id, value }) =>
+              id === 'temperature' ? 'orange' : 'steelblue'
+            }
+            pointSize={10}
+            pointColor={{ theme: 'background' }}
+            pointBorderWidth={2}
+            pointBorderColor={{ from: 'serieColor' }}
+            pointLabel="y"
+            pointLabelYOffset={-12}
+            enablePointLabel={true}
+            enableArea={false}
+            animate={true}
+            motionStiffness={90}
+            motionDamping={15}
+            onClick={(point, event) => {
+              toggleChartType();
+            }}
+            tooltip={({ point }) => {
+              const { serieId, data } = point;
+              const value = serieId === "temperature" ? data.y : data.y;
+          
+              return (
+                <div
+                  style={{
+                    background: "rgba(0, 0, 0, 0.8)",
+                    color: "#fff",
+                    border: "1px solid #000",
+                    padding: "10px",
+                    borderRadius: "5px",
+                  }}
+                >
+                  <strong>{serieId === "temperature" ? "Temperature" : "Wind"}</strong>
+                  <div>
+                    {value}
+                    {serieId === "temperature" ? "°C" : " m/s"}
+                  </div>
+                  <div>Updated: {data.x}</div>
+                </div>
+              );
+            }}
+          />
+
+        </div>
       )}
+
       <div style={{ textAlign: 'center', marginTop: '10px' }}>
         Last updated: {getLatestUpdateTime()}
       </div>
--- Task2/WebApp/giovanniweather/src/data/useWeatherData.js
+++ Task2/WebApp/giovanniweather/src/data/useWeatherData.js
@@ -2,43 +2,60 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useWeatherData = (url) => {
-  const [data, setData] = useState([]);
+const useWeatherData = (url, chartType, country, city) => {
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await axios.get(url);
-      setData([]);
-      const weatherData = formatWeatherData(response.data);
-      setData(weatherData);
+    useEffect(() => {
+        const fetchData = async () => {
+            if (chartType === 'bar') {
+                const response = await axios.get(url + '/GetWeatherForecast');
+                const weatherData = formatWeatherData(response.data);
+
+                setData(weatherData);
+            }
+            else {
+                const utcNow = new Date();
+                const twoHoursAgo = new Date(utcNow.getTime() - 2 * 60 * 60 * 1000);
+
+                const response = await axios.get(url + '/GetWeatherByTimeRange', {
+                    params: {
+                        country: country,
+                        city: city,
+                        from: twoHoursAgo.toISOString(),
+                        to: utcNow.toISOString(),
+                    }
+                });
+
+                setData(response.data);
+            }
+        };
+
+        fetchData();
+        const interval = setInterval(fetchData, 60 * 1000);
+        return () => clearInterval(interval);
+    }, [url, chartType, country, city]);
+
+    const formatWeatherData = (jsonData) => {
+        const groupedData = jsonData.reduce((acc, country) => {
+            country.cities.forEach((city) => {
+                const weather = city.weatherDescriptions[0];
+                const cityData = {
+                    cityName: city.cityName,
+                    countryName: country.countryName,
+                    temperature: weather.temperature,
+                    wind: weather.wind,
+                    lastUpdateDateTime: weather.lastUpdateDateTime,
+                };
+
+                acc.push(cityData);
+            });
+            return acc;
+        }, []);
+
+        return groupedData;
     };
 
-    fetchData();
-    const interval = setInterval(fetchData, 60 * 1000);
-    return () => clearInterval(interval);
-  }, [url]);
-
-  const formatWeatherData = (jsonData) => {
-    const groupedData = jsonData.reduce((acc, country) => {
-      country.cities.forEach((city) => {
-        const weather = city.weatherDescriptions[0];
-        const cityData = {
-          cityName: city.cityName,
-          countryName: country.countryName,
-          temperature: weather.temperature,
-          wind: weather.wind,
-          lastUpdateDateTime: weather.lastUpdateDateTime,
-        };
-  
-        acc.push(cityData);
-      });
-      return acc;
-    }, []);
-  
-    return groupedData;
-  };
-  
-  return data;
+    return data;
 };
 
 export default useWeatherData;
